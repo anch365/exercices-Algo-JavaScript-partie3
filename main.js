83a8e17561c8e341ab050f5b52e4d8612473778b
anch365 <mchindaanchoura@gmail.com>
2026-03-30 13:48:59 +0200

    Boucle foreach

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ for (let index = 0; index <= 5; index++) {
     console.log("*".repeat(index));
 };
 
+// Exercie 2: Boucle foreach
+let heros  = ["Spider-Man", "Batman", "Superman", "Wonder Woman", "Iron Man"];
 
+heros.forEach(hero => {
+    console.log("Je suis fan de " + hero);
+});
 
 
 
